6d172d5bc3adccb28cc1d46c51f1948ce266a092
https://github.com/Mathpatibaswaraj1993/HTMLProject.git <mathpatibaswaraj@gmail.com>
2025-09-09 15:41:39 +0530

    changes 3

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import img1 from "../assets/p1.png";
-import img2 from "../assets/P2.png";
-import img3 from "../assets/P3.png";
-import img4 from "../assets/P4.png";
+import img2 from "../assets/p2.png";
+import img3 from "../assets/p3.png";
+import img4 from "../assets/p4.png";
 function Showcase() {
   const images = [
     {

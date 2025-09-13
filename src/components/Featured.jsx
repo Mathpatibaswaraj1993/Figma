aa69d13cacbecf68cdc94473633ac39f42ca817d
https://github.com/Mathpatibaswaraj1993/HTMLProject.git <mathpatibaswaraj@gmail.com>
2025-09-13 13:26:44 +0530

    changes 2

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -4,7 +4,7 @@ const Featured = ({ icon, title, description }) => {
   return (
     <div className="p-6 border border-white rounded-lg bg-transparent">
       {/* SVG icon */}
-      <img src={icon} alt="" className="h-10 w-10 mb-4" />
+      <img src={icon} alt="" className="h-10 w-10 mb-4 bg-white" />
       <h3 className="text-xl font-semibold mb-2">{title}</h3>
       <p className="text-sm">{description}</p>
     </div>
